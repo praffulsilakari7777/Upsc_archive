bfff53a5ba6cadb463e2afcf40118d036b56389d
praffulsilakari7777 <60846152+praffulsilakari7777@users.noreply.github.com>
2021-03-14 11:29:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((error, req, res, next) => {
   useNewUrlParser: true
  })
  .then(() => {
-   app.listen(process.env.PORT || 80);
+   app.listen(80);
    console.log('DB connection successful!');
  })
  .catch(err => {
